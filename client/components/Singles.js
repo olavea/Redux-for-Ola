ce4182aa80f998185fc1aa347796076a8eae3611
Ola Vea <ola@olavea.com>
2017-12-29 19:39:45 +0100

    Copied from Wes Bos
    Copied from Wes Bos 
    
    // from Wes Bos video 13: Displaying the Single Photo Component
    // We already are displaying a list of photos, but what about when you click through to the Single photo component? 
    
    
    https://github.com/wesbos/Learn-Redux-Starter-Files/blob/master/14/components/Single.js

--- client/components/Singles.js
+++ client/components/Singles.js
@@ -1,13 +1,29 @@
+
 import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
 
 const Single = React.createClass({
   render() {
+
+    const { postId } = this.props.params;
+    // index of post
+    const i = this.props.posts.findIndex((post) => post.code === postId);
+    // get us the post
+    const post = this.props.posts[i];
+    // console.log(post);
+    const postComments = this.props.comments[postId] || [];
+
     return (
       <div className="single-photo">
-      I'm the single
+        <Photo i={i} post={post} {...this.props} />
+        <Comments postComments={postComments} />
       </div>
     )
   }
 });
 
 export default Single;
+
+// from Wes Bos video 13: Displaying the Single Photo Component
+// We already are displaying a list of photos, but what about when you click through to the Single photo component? 
