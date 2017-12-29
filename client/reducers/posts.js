9dc77f7086bab893e22be74640cd9455172ab153
Ola Vea <ola@olavea.com>
2017-12-29 19:31:07 +0100

    Copied  from Wes Bos
    Copied  from Wes Bos 
    
    
    // from Wes Bos video 12: Updating State with Reducers
    // Once the action has been dispatched, we can use reducers to update our state.
    
    
    https://github.com/wesbos/Learn-Redux-Starter-Files/blob/master/13/reducers/posts.js

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -6,7 +6,19 @@
 function posts(state = [], action) {
   //console.log("The post will change");
   //console.log(state, action);
-  return state;
+  switch(action.type) {
+    case 'INCREMENT_LIKES' :
+
+      console.log("Incrementing Likes!!");
+      // return the updated state
+      const i = action.index;
+      return [
+        ...state.slice(0,i), // before the one we are updating
+        {...state[i], likes: state[i].likes + 1},
+        ...state.slice(i + 1), // after the one we are updating
+      ]
+    default:
+      return state;
 }
 
 export default posts;
@@ -19,3 +31,9 @@ export default posts;
 //return: store
 
 // Video 9: Every time you dispatch an action, every single reducer will run. 3:00
+// Video 12: in Reduc we use functional programming and what is really important about that is that we do not mutate our state
+// Video 12: A pure function is a predictable function, <Wes goes into codepen.io to code an impure function: gets a different result every time.
+// Video 12: What we can do isto fix this we take a copy and return the new state
+// Video 12: We get the same result every time, we don't mutate an external state
+// from Wes Bos video 12: Updating State with Reducers
+// Once the action has been dispatched, we can use reducers to update our state.
