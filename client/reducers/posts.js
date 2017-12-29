c459cc11b32b1c7db4cffa762994e6aba16de1bf
Ola Vea <ola@olavea.com>
2017-12-29 18:25:05 +0100

    Update posts.js
    // action(what happenededd), store
    
    //ok let me see
    
    //return: store

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -3,6 +3,13 @@
 // 1. the action (info about what happened)
 // 2. copy of current state
 
+// action(what happenededd), store
+
+//ok let me see
+
+//return: store
+
+
 function posts(state = [], action) {
   console.log(state, action);
   return state;
